29d9d369eed49aeddd5a5dfe0aa76f5061f65eb9
smtswaraj <swarajnayakdipu227@gmail.com>
2025-08-03 17:58:08 +0530

    add defaultdict

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from pathlib import Path
+# from pathlib import Path
 
-file = Path("./files/Sample.txt")
-if file.exists():
-    print(file.read_text())
-else:
-    file.write_text("Hello, World!")
+# file = Path("./files/Sample.txt")
+# if file.exists():
+#     print(file.read_text())
+# else:
+#     file.write_text("Hello, World!")
 # ------------------------------------------------------------------
 
 # from functools import singledispatch
@@ -25,3 +25,42 @@ else:
 
 # -------------------------------------------------------------------
 
+# import timeit
+# def fun():
+#     return [i for i in range(1000)]
+# def fun1():
+#     return list(range(1000))
+
+# print(timeit.timeit(fun, number=10000))
+# print(timeit.timeit(fun1, number=10000))
+
+# --------------------------------------------------------------------
+
+# from dataclasses import dataclass
+
+# @dataclass
+# class Book:
+#     title: str
+#     author: str
+#     year: int
+# b = Book("STM", "George Orwell", 1949)
+# print(b.title)
+
+# ---------------------------------------------------------------------
+
+# lst = [("apple", 2), ("banana", 3), ("apple", 4)]
+# group = {}
+# for i in lst:
+#     if group.get(i[0]):
+#         group[i[0]].append(i[1])
+#     else:
+#         group[i[0]] = [i[1]]
+# print(group)
+
+# -----------BY using the `collections.defaultdict`--------------
+# from collections import defaultdict
+# lst = [("apple", 2), ("banana", 3), ("apple", 4)]
+# group = defaultdict(list)
+# for k, v in lst:
+#     group[k].append(v)
+# print(dict(group))
